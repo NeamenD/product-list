70c775604a8a82a9cc01286ff36ba6b71c1e56c5
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-05-09 14:13:22 -0600

    setup state and passed it as props

--- src/components/FilterableProductTable.jsx
+++ src/components/FilterableProductTable.jsx
@@ -1,11 +1,19 @@
 import SearchBar from "./SearchBar";
 import ProductTable from "./ProductTable";
+import { useState } from "react";
 
 function FilterableProductTable({ products }) {
+  const [filterText, setFilterText] = useState("");
+  const [inStockOnly, setInStockOnly] = useState(false);
+
   return (
     <>
-      <SearchBar />
-      <ProductTable products={products} />
+      <SearchBar filterText={filterText} inStockOnly={inStockOnly} />
+      <ProductTable
+        products={products}
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+      />
     </>
   );
 }
